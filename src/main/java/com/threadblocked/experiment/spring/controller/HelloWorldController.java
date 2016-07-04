09d14529d44c0755f8eb916239b4a515f7f955c6
Arthur Tsang <kingarthur0902@gmail.com>
2016-07-04 23:03:30 +0800

    downgrade spring

--- src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
+++ src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
@@ -2,16 +2,31 @@ package com.threadblocked.experiment.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ArthurTsang on 7/4/16.
  */
+@RequestMapping(value = "/hello")
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping(value = "/helloworld")
+    /**
+     * hello world
+     * @param model
+     */
+    @RequestMapping(value = "/world")
     public void helloWorld(Model model){
         System.out.println("aaaaaaaaaaaaaaaa");
+    }
+
+    /**
+     * uri 模板
+     * @param id
+     */
+    @RequestMapping(value = "/{id}")
+    public void hello(@PathVariable String id){
+        System.out.println("id : " + id);
     }
 }
