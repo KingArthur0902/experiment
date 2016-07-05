c3215e0e3138aaa994499efdd72eef108e79b394
Arthur Tsang <kingarthur0902@gmail.com>
2016-07-06 00:01:23 +0800

    request mapping test

--- src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
+++ src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.threadblocked.experiment.spring.controller;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +24,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by ArthurTsang on 7/4/16.
@@ -64,7 +71,6 @@ public class HelloWorldController {
         writer.write(body);
     }
 
-
     /**
      * 返回body
      * @return
@@ -73,12 +79,17 @@ public class HelloWorldController {
     public String hello4(){
         return "helloWorld";
     }
+
+    @ModelAttribute
+    public int getAaa(){
+        return 123;
+    }
     /**
      * 使用@ModelAttribute注解的方法可以在本controller方法调用前执行,可以存放一些共享变量,如枚举值,或是一些初始化操作
      */
     @ModelAttribute
     public void init(ModelMap model){
-        model.put("aaa",123);
+        model.put("aaa",getAaa());
     }
 
     /**
@@ -89,9 +100,16 @@ public class HelloWorldController {
     public void hello5(ModelMap model){
         System.out.println(model.get("aaa"));
     }
-
     /**
-     * 获取请求头部书记胡
+     * 使用ModelAttribute 获得数据
+     * @param aaa
+     */
+    @RequestMapping(value = "/something3/aaa")
+    public void hello5(@ModelAttribute String aaa){
+        System.out.println(aaa);
+    }
+    /**
+     * 获取请求头部数据
      * @param encoding
      * @param host
      */
@@ -100,6 +118,38 @@ public class HelloWorldController {
                                   @RequestHeader("Host") String host) {
         System.out.println(encoding);
         System.out.println(host);
+    }
+
+    /**
+     * 接收矩阵变量
+     * @param vars
+     * @param avars
+     */
+    @RequestMapping(value = "/something5/{sid}/anything/{aid}")
+    public void hello7(@MatrixVariable Map<String,String> vars,@MatrixVariable(pathVar = "aid") Map<String,String> avars){
+        System.out.println(vars);
+        System.out.println(avars);
+    }
+
+    /**
+     * 使用HttpEntity
+     * @param requestEntity
+     */
+    @RequestMapping(value = "/something6")
+    public ResponseEntity<String> hello8(HttpEntity<byte[]> requestEntity){
+        String requestHeader = requestEntity.getHeaders().getFirst("myRequestHeader");
+        byte[] requestBody = requestEntity.getBody();
+        System.out.println(requestHeader);
+        System.out.println(requestBody);
+
+        HttpHeaders responeHeaders = new HttpHeaders();
+        responeHeaders.set("myResponseHeader","bbbbbbbbbbb");
+        return new ResponseEntity<String>("hello world",responeHeaders, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/something7")
+    public void hello9(@CookieValue String JSESSIONID){
+        System.out.println(JSESSIONID);
     }
 
 
