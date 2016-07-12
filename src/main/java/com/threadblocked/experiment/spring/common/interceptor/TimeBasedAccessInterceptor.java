9db0f6358f20c45ef9a923ffb3d3272a8b7a30ec
Arthur Tsang <kingarthur0902@gmail.com>
2016-07-12 22:03:45 +0800

    add content negotiate

---
+++ src/main/java/com/threadblocked/experiment/spring/common/interceptor/TimeBasedAccessInterceptor.java
@@ -0,0 +1,44 @@
+package com.threadblocked.experiment.spring.common.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+/**
+ * 这是一个spring的拦截器，拦截请求，请求前先判断一个时间范围
+ * 参考自spring 官方文档
+ * Created by ArthurTsang on 7/12/16.
+ */
+public class TimeBasedAccessInterceptor extends HandlerInterceptorAdapter {
+    private int openingTime;
+    private int closingTime;
+
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler) throws Exception{
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if(openingTime <= hour && hour < closingTime){
+            return true;
+        }
+        response.sendRedirect("http://www.google.com");
+        return false;
+    }
+
+    public int getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(int openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public int getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(int closingTime) {
+        this.closingTime = closingTime;
+    }
+}
--- src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
+++ src/main/java/com/threadblocked/experiment/spring/controller/HelloWorldController.java
@@ -37,121 +37,134 @@ public class HelloWorldController {
 
     /**
      * hello world
+     *
      * @param model
      */
     @RequestMapping(value = "/world")
-    public void hello0(Model model){
+    public void hello0(Model model) {
         System.out.println("aaaaaaaaaaaaaaaa");
     }
 
     /**
      * uri 模板
+     *
      * @param id
      */
     @RequestMapping(value = "/{id}")
-    public void hello1(@PathVariable String id){
+    public void hello1(@PathVariable String id) {
         System.out.println("id : " + id);
     }
 
     /**
      * 传参数
+     *
      * @param name
      */
     @RequestMapping(value = "/something0")
-    public void hello2(@RequestParam String name){
+    public void hello2(@RequestParam String name) {
         System.out.println(name);
     }
 
     /**
      * 请求body
+     *
      * @param body
      * @param writer
      * @throws IOException
      */
-    @RequestMapping(value = "/something1",method = RequestMethod.PUT)
+    @RequestMapping(value = "/something1", method = RequestMethod.PUT)
     public void hello3(@RequestBody String body, Writer writer) throws IOException {
         writer.write(body);
     }
 
     /**
      * 返回body
+     *
      * @return
      */
-    @RequestMapping(value = "/something2",method = RequestMethod.GET)
-    public String hello4(){
+    @RequestMapping(value = "/something2", method = RequestMethod.GET)
+    public String hello4() {
         return "helloWorld";
     }
 
     @ModelAttribute
-    public int getAaa(){
+    public int getAaa() {
         return 123;
     }
+
     /**
      * 使用@ModelAttribute注解的方法可以在本controller方法调用前执行,可以存放一些共享变量,如枚举值,或是一些初始化操作
      */
     @ModelAttribute
-    public void init(ModelMap model){
-        model.put("aaa",getAaa());
+    public void init(ModelMap model) {
+        model.put("aaa", getAaa());
     }
 
     /**
      * 使用ModelAttribute 获得数据
+     *
      * @param model
      */
     @RequestMapping(value = "/something3")
-    public void hello5(ModelMap model){
+    public void hello5(ModelMap model) {
         System.out.println(model.get("aaa"));
-        model.addAttribute("aaa",222);
+        model.addAttribute("aaa", 222);
     }
+
     /**
      * 使用ModelAttribute 获得数据
+     *
      * @param aaa
      */
     @RequestMapping(value = "/something3/aaa")
-    public void hello5(@ModelAttribute String aaa){
+    public void hello5(@ModelAttribute String aaa) {
         System.out.println(aaa);
     }
+
     /**
      * 获取请求头部数据
+     *
      * @param encoding
      * @param host
      */
     @RequestMapping(value = "/something4")
     public void hello6(@RequestHeader("Accept-Encoding") String encoding,
-                                  @RequestHeader("Host") String host) {
+                       @RequestHeader("Host") String host) {
         System.out.println(encoding);
         System.out.println(host);
     }
 
     /**
      * 接收矩阵变量
+     *
      * @param vars
      * @param avars
      */
     @RequestMapping(value = "/something5/{sid}/anything/{aid}")
-    public void hello7(@MatrixVariable Map<String,String> vars,@MatrixVariable(pathVar = "aid") Map<String,String> avars){
+    public void hello7(@MatrixVariable Map<String, String> vars, @MatrixVariable(pathVar = "aid") Map<String, String> avars) {
         System.out.println(vars);
         System.out.println(avars);
     }
 
     /**
      * 使用HttpEntity
+     *
      * @param requestEntity
      */
     @RequestMapping(value = "/something6")
-    public ResponseEntity<String> hello8(HttpEntity<byte[]> requestEntity){
+    public ResponseEntity<String> hello8(HttpEntity<byte[]> requestEntity) {
         String requestHeader = requestEntity.getHeaders().getFirst("myRequestHeader");
         byte[] requestBody = requestEntity.getBody();
         System.out.println(requestHeader);
         System.out.println(requestBody);
 
         HttpHeaders responeHeaders = new HttpHeaders();
-        responeHeaders.set("myResponseHeader","bbbbbbbbbbb");
-        return new ResponseEntity<String>("hello world",responeHeaders, HttpStatus.CREATED);
+        responeHeaders.set("myResponseHeader", "bbbbbbbbbbb");
+        return new ResponseEntity<String>("hello world", responeHeaders, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/something7")
-    public void hello9(@CookieValue String JSESSIONID){
+    public void hello9(@CookieValue String JSESSIONID) {
         System.out.println(JSESSIONID);
     }
 
